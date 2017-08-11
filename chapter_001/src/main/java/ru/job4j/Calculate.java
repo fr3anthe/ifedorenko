b248df1e4a0a8ca38f21de193702eeede973387e
fr3anthe <if.zommy@gmail.com>
2017-08-11 22:27:06 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,19 @@
 package ru.job4j;
 
+/**
+ *Class Класс для вывода приветствия
+ *@author ifedorenko
+ *@since 11.08.2017
+ *@version 1
+ */
+
+
 public class Calculate
 {
+	/**
+	* Конструктор, вывод строки в консоль.
+	*@param arg - arg.
+	*/
 	public static void main(String[] args)
 	{
 		System.out.println("Hello world");
