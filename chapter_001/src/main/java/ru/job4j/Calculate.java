3ae1dabe4edfb4860705103c284c90a4ac27ea33
fr3anthe <if.zommy@gmail.com>
2017-08-13 13:40:09 +0300

    1.5. Checkstyle [#4123]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,21 +1,19 @@
 package ru.job4j;
 
 /**
- *Class Класс для вывода приветствия
+ *Class Класс для вывода приветствия.
  *@author ifedorenko
  *@since 11.08.2017
  *@version 1
  */
 
 
-public class Calculate
-{
+public class Calculate {
 	/**
 	* Конструктор, вывод строки в консоль.
-	*@param arg - arg.
+	*@param args - args.
 	*/
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
 }
