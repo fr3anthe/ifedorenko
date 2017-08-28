1accfa061edde9f8daad17f143c4951fd4c59a91
fr3anthe <if.zommy@gmail.com>
2017-08-28 16:39:39 +0300

    3. Используя класс StubInput написать тесты проверяющие поведение пользователя[#23127]

--- chapter_002/src/test/java/ru/job4j/tracker/StubInputTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StubInputTest.java
@@ -26,8 +26,12 @@ public class StubInputTest {
     @Test
     public void whenShowAllThenTrackerReturnAllExistItems() {
         Tracker tracker = new Tracker();
-        Input input = new StubInput(new String[]{"0", "Igor", "desc1", "0", "Dima", "desc2", "1", "6"});
+		Item item1 = tracker.add(new Item());
+		Item item2 = tracker.add(new Item());
+        Input input = new StubInput(new String[]{"1", "6"});
         new StartUI(input, tracker).allAction();
+		Item[] except = {item1, item2};
+		assertThat(tracker.findAll(), is(except));
 
     }
     /**
@@ -62,6 +66,7 @@ public class StubInputTest {
         Item item = tracker.add(new Item());
         Input input = new StubInput(new String[]{"4", item.getId(), "6"});
         new StartUI(input, tracker).allAction();
+        assertThat(tracker.findById(tracker.findAll()[0].getId()), is(item));
     }
 
     /**
@@ -70,7 +75,10 @@ public class StubInputTest {
     @Test
     public void whenUserFindByNameThenReturnAllItemsWithOurName() {
         Tracker tracker = new Tracker();
-        Input input = new StubInput(new String[]{"0", "igor", "desc1", "0", "dima", "desc2", "0", "igor", "desc3", "5", "igor", "6"});
+        Item item1 = tracker.add(new Item("Igor", "desc1", System.currentTimeMillis()));
+        Item item2 = tracker.add(new Item("Dasha", "desc2", System.currentTimeMillis()));
+        Input input = new StubInput(new String[]{"5", "Igor", "6"});
         new StartUI(input, tracker).allAction();
+        assertThat(tracker.findByName("Igor")[0], is(item1));
     }
 }
