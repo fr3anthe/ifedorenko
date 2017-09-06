002a88f4b76065881dcd909d07fc4e9ff4a4208f
fr3anthe <if.zommy@gmail.com>
2017-09-06 13:07:03 +0300

    Update main POM + create chapter_003 add new class CollectionsTime

---
+++ chapter_003/src/main/java/ru/job4j/lite/CollectionsTime.java
@@ -0,0 +1,44 @@
+package ru.job4j.lite;
+
+import java.util.Collection;
+
+/**
+ *Класс CollectionsTime.
+ *@author ifedorenko
+ *@since 06.09.2017
+ *@version 1
+ */
+public class CollectionsTime {
+    /**
+     * Метод проверяет за сколько происходит операция add в ArrayList, LinkedList, TreeSet.
+     * @param collection Коллекция для проверки
+     * @param amount кол-во элементов для проверки
+     * @return время работы
+     */
+    public long add(Collection<String> collection, int amount) {
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < amount; i++) {
+            String s = String.valueOf(i);
+            collection.add(s);
+        }
+        long finish = System.currentTimeMillis();
+        long result = finish - start;
+        return result;
+    }
+    /**
+     * Метод проверяет за сколько происходит операция delete в ArrayList, LinkedList, TreeSet.
+     * @param collection Коллекция для проверки
+     * @param amount кол-во элементов для проверки
+     * @return время работы
+     */
+    public long delete(Collection<String> collection, int amount) {
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < amount; i++) {
+            String s = String.valueOf(i);
+            collection.remove(s);
+        }
+        long finish = System.currentTimeMillis();
+        long result = finish - start;
+        return result;
+    }
+}
---
+++ chapter_003/src/main/java/ru/job4j/lite/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for Collections.
+*
+* @author Igor Fedorenko (mailto:if.zommy@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.lite;
