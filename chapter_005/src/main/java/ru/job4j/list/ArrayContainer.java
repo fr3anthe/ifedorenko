4545719cc6f7cdf4ff86f574ba6f09f2c001fb7d
fr3anthe <if.zommy@gmail.com>
2017-09-26 10:38:48 +0300

    5.3.1. Создать динамический список на базе массива. [#158]

---
+++ chapter_005/src/main/java/ru/job4j/list/ArrayContainer.java
@@ -0,0 +1,115 @@
+package ru.job4j.list;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Class ArrayContainer.
+ * @param <E> generic
+ */
+public class ArrayContainer<E> implements SimpleContainer {
+    /**
+     * @param DEFAULT_CAPACITY default size of array
+     */
+    private static final int DEFAULT_CAPACITY = 10;
+    /**
+     * @param EMPTY_ELEMENTDATA size array when initialize by null
+     */
+    private static final Object[] EMPTY_ELEMENTDATA = {};
+    /**
+     * @param objects our container
+     */
+    private Object[] objects;
+    /**
+     * @param index position in array
+     */
+    private int index = 0;
+
+    /**
+     * Constructor.
+     * @param initialCapacity start size.
+     */
+    public ArrayContainer(int initialCapacity) {
+        if (initialCapacity > 0) {
+            this.objects = new Object[initialCapacity];
+        } else if (initialCapacity == 0) {
+            this.objects = EMPTY_ELEMENTDATA;
+        } else {
+            throw new IllegalArgumentException("Illegal Capacity: " + initialCapacity);
+        }
+    }
+
+    /**
+     * Default constructor.
+     */
+    public ArrayContainer() {
+        this.objects = new Object[DEFAULT_CAPACITY];
+    }
+
+    /**
+     * Method add.
+     * @param o object for adding
+     */
+    @Override
+    public void add(Object o) {
+        if (index >= objects.length) {
+            objects = Arrays.copyOf(objects, objects.length * 2);
+            objects[index++] = o;
+        } else {
+            objects[index++] = o;
+        }
+    }
+
+    /**
+     * Method get.
+     * @param index position for return.
+     * @return object.
+     */
+    @Override
+    public E get(int index) {
+        E result = null;
+        if (index >= objects.length) {
+            throw new IndexOutOfBoundsException();
+        } else {
+            result = (E) objects[index];
+        }
+        return result;
+    }
+
+    /**
+     * Iterator.
+     * @return iterator
+     */
+    @Override
+    public Iterator iterator() {
+        Iterator<E> iterator = new Iterator<E>() {
+            /**
+             * @param position position in array
+             */
+            private int position = 0;
+
+            /**
+             * Method hasNext.
+             * @return result
+             */
+            @Override
+            public boolean hasNext() {
+                return position < objects.length;
+            }
+
+            /**
+             * Method next.
+             * @return object
+             */
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return (E) objects[position++];
+            }
+        };
+        return iterator;
+    }
+}
---
+++ chapter_005/src/test/java/ru/job4j/list/ArrayContainerTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.list;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Class ArrayContainerTest.
+ */
+public class ArrayContainerTest {
+    /**
+     * Test method add and get.
+     */
+    @Test
+    public void whenAddElementInContainterThenGetThisElement() {
+        SimpleContainer<Integer> ac = new ArrayContainer<>(6);
+        ac.add(5);
+        ac.add(4);
+        ac.add(3);
+        ac.add(7);
+        ac.add(9);
+        ac.add(4);
+        Integer result = ac.get(5);
+        assertThat(result, is(4));
+
+    }
+}
