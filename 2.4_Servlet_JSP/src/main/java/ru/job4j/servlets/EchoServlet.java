c8c570a1baac93be6699271d4a772b9de5dfc7ce
fr3anthe <if.zommy@gmail.com>
2018-07-25 23:14:10 +0300

    Создать web проект

--- 2.4_Servlet_JSP/src/main/java/ru/job4j/servlets/EchoServlet.java
+++ 2.4_Servlet_JSP/src/main/java/ru/job4j/servlets/EchoServlet.java
@@ -3,6 +3,30 @@ package ru.job4j.servlets;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class EchoServlet {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+/**
+ * EchoServlet.
+ * @author ifedorenko
+ * @since 25.07.2018
+ */
+public class EchoServlet extends HttpServlet {
+    /**
+     * Logger.
+     */
     private static final Logger LOGGER = LoggerFactory.getLogger(EchoServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = new PrintWriter(resp.getOutputStream());
+        writer.append("hello world");
+        writer.flush();
+    }
 }
