2ee82ab55ed615d1370495616fcc6e8ab32ef139
fr3anthe <if.zommy@gmail.com>
2018-07-25 23:12:29 +0300

    Removed .idea, .iml

---
+++ 2.4_Servlet_JSP/src/main/java/ru/job4j/servlets/EchoServlet.java
@@ -0,0 +1,8 @@
+package ru.job4j.servlets;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EchoServlet {
+    private static final Logger LOGGER = LoggerFactory.getLogger(EchoServlet.class);
+}
