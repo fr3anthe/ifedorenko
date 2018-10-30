306b0da8e33fcb427ca31ac7c81add3b540e9da6
fr3anthe <if.zommy@gmail.com>
2018-10-30 20:31:18 +0300

    update personal blog

---
+++ Personal/src/main/java/ru/job4j/javacode/Test.java
@@ -0,0 +1,17 @@
+package ru.job4j.javacode;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Class Test
+ *
+ * @author ifedorenko
+ * @since 23.10.2018
+ */
+public class Test {
+    /**
+     * Logger.
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(Test.class);
+}
