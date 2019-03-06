343df1909c010b46e726ac8a2ccddf9f04fbc813
fr3anthe <if.zommy@gmail.com>
2019-03-06 21:16:10 +0300

    local update

--- 2.4-Servlet-JSP/src/main/java/ru/job4j/servlets/http/SigninController.java
+++ 2.4-Servlet-JSP/src/main/java/ru/job4j/servlets/http/SigninController.java
@@ -5,7 +5,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 import java.io.IOException;
 
-@WebServlet(name = "SigninController")
+@WebServlet(name = "SigninController", urlPatterns = "/signin", loadOnStartup = 1)
 public class SigninController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
